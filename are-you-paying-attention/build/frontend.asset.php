0d92dc1aa051dd93da20aae644daaa1597fcdd95
nk44ukrnet <56731417+nk44ukrnet@users.noreply.github.com>
2024-02-07 16:49:35 +0200

    Add files via upload

---
+++ are-you-paying-attention/build/frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'e6c74e4d564964dbe29f');
---
+++ are-you-paying-attention/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components'), 'version' => 'f0e7d424c5f0e6670adc');
